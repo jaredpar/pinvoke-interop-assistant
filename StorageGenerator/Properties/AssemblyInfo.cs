3380ce863dfdda21125542770aca6931a3c1300b
Nordin Rahman <nordin.rahman@gmail.com>
2016-08-21 10:36:18 +0800

    Add AssemblyInfo code file to StorageGenerator for consistency

---
+++ StorageGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("7d27b6fe-ea66-4187-b202-da50c8a927de")]
+
+[assembly: AssemblyTitle("StorageGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PInvoke")]
+[assembly: AssemblyCopyright("Copyright ©  2006")]
+
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.0.*")> 
+
+[assembly: AssemblyVersion(PInvoke.Constants.Version)]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
